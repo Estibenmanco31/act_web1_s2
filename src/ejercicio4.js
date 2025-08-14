e7071480e8aa9014bf8b3d07f95b65a8eea473b5
Estiben Manco <estibenmanco1031@gmail.com>
2025-08-14 12:01:11 -0500

    ejercicio 4 resuelto

---
+++ src/ejercicio4.js
@@ -0,0 +1,29 @@
+//Calculadora basica con switch
+let a = 10;
+let b = 3;
+let operador = '*';
+let resultado;
+switch (operador) {
+  case '+':
+    resultado = a + b;
+    console.log(resultado);
+    break;
+  case '-':
+    resultado = a - b;
+    console.log(resultado);
+    break;
+  case '*':
+    resultado = a * b;
+    console.log(resultado);
+    break;
+  case '/':
+    if (b === 0) {
+      console.log("indeterminado");
+    } else {
+      resultado = a / b;
+      console.log(resultado);
+    }
+    break;
+  default:
+    console.log("operador no válido");
+}
