36f19ee4f295a9be77a296a7003a4b1b5512d3f3
Estiben Manco <estibenmanco1031@gmail.com>
2025-08-14 12:06:47 -0500

    ejercicio 8 resuelto

---
+++ src/ejercicio8.js
@@ -0,0 +1,20 @@
+//Transporte con tarifa reducida
+let edad = 67;
+let distanciaKm = 120;
+let tarifaBase = distanciaKm * 0.21;
+let descuento = 0;
+let precioFinal;
+
+if (edad < 18) {
+    descuento = 0.20; 
+} else if (edad >= 65) {
+    descuento = 0.40; 
+} else {
+    descuento = 0; 
+}
+
+precioFinal = tarifaBase - (tarifaBase * descuento);
+precioFinal = Math.round(precioFinal * 100) / 100;
+
+console.log(precioFinal);
+
