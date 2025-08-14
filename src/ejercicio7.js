9d97cb0d4ad80aa86f258511ab303da1d2a2fd21
Estiben Manco <estibenmanco1031@gmail.com>
2025-08-14 12:05:43 -0500

    ejercicio 7 resuelto

---
+++ src/ejercicio7.js
@@ -0,0 +1,19 @@
+//Descuento por volumen
+let precioUnitario = 25;
+let cantidad = 60;
+let precioFinal;
+let total = precioUnitario * cantidad;
+let descuento = 0;
+
+if (cantidad < 10) {
+    descuento = 0;
+} else if (cantidad >= 10 && cantidad < 50) {
+    descuento = 0.10;
+} else if (cantidad >= 50) {
+    descuento = 0.20; 
+}
+
+precioFinal = total - (total * descuento);
+precioFinal = Math.round(precioFinal * 100) / 100;
+
+console.log(precioFinal);
