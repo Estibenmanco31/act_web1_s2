fb97e25104c8bd5fcaf84055e22af76cfaad4b2e
Estiben Manco <estibenmanco1031@gmail.com>
2025-08-14 12:02:55 -0500

    ejercicio 5 resuelto

---
+++ src/ejercicio5.js
@@ -0,0 +1,21 @@
+//Dia laborable o fin de semana
+let dia = 'Lunes';
+
+dia = dia.toLowerCase();
+
+switch (dia) {
+    case 'lunes':
+    case 'martes':
+    case 'miercoles':
+    case 'miércoles':
+    case 'jueves':
+    case 'viernes':
+        console.log("laborable");
+        break;
+    case 'sabado':
+    case 'domingo':
+        console.log("fin de semana");
+        break;
+    default:
+        console.log("Día no válido");
+}
